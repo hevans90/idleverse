1cc6c6897a79908f1523216df0e462d7d417b3de
Constantin Christoforidis <MechaKnightz@hotmail.com>
2021-09-29 18:54:10 +0200

    fix formatting

--- apps/web/src/app/containers/self-container.tsx
+++ apps/web/src/app/containers/self-container.tsx
@@ -6,7 +6,7 @@ import { selfVar } from '../_state/reactive-variables';
 export const SelfContainer = (props: any) => {
   const { loading, error, data } = useQuery<SelfQuery>(SelfDocument, {
     onCompleted: (data) => {
-      console.log("done");
+      console.log('done');
       if (data && data.user_me && data.user_me[0]) {
         selfVar(data.user_me[0]);
       }
@@ -14,10 +14,6 @@ export const SelfContainer = (props: any) => {
     fetchPolicy: 'network-only',
   });
 
-  console.log("loading")
-  console.log(loading);
-  console.log(data);
-  console.log(error)
   if (loading || !data) return <Loading></Loading>;
   return props.children;
 };
