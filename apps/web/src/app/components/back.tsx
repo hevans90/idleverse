9a880f3b414781cb65a022ad98d7a269e91e44a0
Harri Evans <hevans9000@gmail.com>
2021-09-30 17:34:19 +0100

    fix(web): back button

--- apps/web/src/app/components/back.tsx
+++ apps/web/src/app/components/back.tsx
@@ -6,22 +6,24 @@ export const Back = () => {
   const border = useColorModeValue('gray.200', 'gray.600');
 
   return (
-    <Box
-      padding="1rem"
-      display="flex"
-      flexDirection="column"
-      position="absolute"
-      alignItems="start"
-      bgColor={bg}
-      top="0"
-      left="0"
-      borderWidth="1px"
-      borderStyle="solid"
-      borderColor={border}
-      borderLeft="unset"
-      borderTop="unset"
-    >
-      <Link to="/">Back</Link>
-    </Box>
+    <Link to="/">
+      <Box
+        padding="1rem"
+        display="flex"
+        flexDirection="column"
+        position="absolute"
+        alignItems="start"
+        bgColor={bg}
+        top="0"
+        left="0"
+        borderWidth="1px"
+        borderStyle="solid"
+        borderColor={border}
+        borderLeft="unset"
+        borderTop="unset"
+      >
+        Back
+      </Box>
+    </Link>
   );
 };
