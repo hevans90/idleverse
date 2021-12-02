7a7eedd1bda22f5166f8955a3386aad00f984df4
Tristan Dickson <tristandickson@gmail.com>
2021-12-02 15:17:00 +0000

    added diner and animations and plaions and recruitment drawer

--- apps/food-client/src/app/canvases/solar-system/solar-system.tsx
+++ apps/food-client/src/app/canvases/solar-system/solar-system.tsx
@@ -1,6 +1,7 @@
 import { Box } from '@chakra-ui/react';
+import { render } from '@inlet/react-pixi';
 import { Application } from '@pixi/app';
-import { Graphics, Sprite, Texture } from 'pixi.js';
+import * as PIXI from 'pixi.js';
 import { useEffect } from 'react';
 
 type Vector2D = {
@@ -11,7 +12,7 @@ type Vector2D = {
 type Tile = {
   x: number;
   y: number;
-  occupant?: Sprite;
+  occupant?: PIXI.Sprite;
 };
 
 type TileConfig = string[][];
@@ -24,6 +25,22 @@ type Road = {
   connections: number[];
 };
 
+type Drawer = {
+  body: {
+    height: number;
+    width: number;
+  };
+  tab;
+};
+
+type Diner = {
+  time: number;
+  duration: number;
+  previousPosition: number;
+  nextPosition: number;
+  update?: () => void;
+};
+
 export const SolarSystem = () => {
   useEffect(() => {
     const gameElement = document.getElementById('game');
@@ -32,6 +49,41 @@ export const SolarSystem = () => {
       resizeTo: gameElement,
     });
     app.stage.sortableChildren = true;
+
+    const animations = [];
+    const roads = [];
+    const houses = [];
+
+    const isValidPosition = (position) => {
+      if (
+        roads.filter((road) => road.i === position.i && road.j === position.j)
+          .length > 0
+      )
+        return false;
+      if (
+        houses.filter(
+          (house) => house.i === position.i && house.j === position.j
+        ).length > 0
+      )
+        return false;
+      if (
+        roads.filter(
+          (road) =>
+            Math.abs(road.i - position.i) +
+            Math.abs(road.j - position.j) < 2
+        ).length === 0
+      )
+        return false;
+
+      return true;
+    };
+
+    const getRandomPosition = () => {
+      return {
+        i: Math.floor(Math.random() * 10),
+        j: Math.floor(Math.random() * 10),
+      };
+    };
 
     const ts = 80;
     const ts1_3 = ts / 8;
@@ -51,7 +103,7 @@ export const SolarSystem = () => {
       ['e', 'e', 'r24', 'e', 'e'],
       ['e', 'e', 'r24', 'h4', 'e'],
       ['r13', 'r13', 'r124', 'e', 'e'],
-      ['e', 'e', 'r24', 'e', 'e'],
+      ['d', 'e', 'r24', 'e', 'e'],
       ['e', 'e', 'r24', 'e', 'e'],
     ];
 
@@ -72,7 +124,7 @@ export const SolarSystem = () => {
     ];
 
     const drawEmptySquare = (x: number, y: number) => {
-      const square = new Graphics();
+      const square = new PIXI.Graphics();
       square.lineStyle(2, 0x0, 1);
       square.beginFill(0xffffff);
       square.drawRect(2, 2, ts - 2, ts - 2);
@@ -84,13 +136,17 @@ export const SolarSystem = () => {
       return square;
     };
 
-    const drawLine = (graphic: Graphics, start: Vector2D, end: Vector2D) => {
+    const drawLine = (
+      graphic: PIXI.Graphics,
+      start: Vector2D,
+      end: Vector2D
+    ) => {
       graphic.moveTo(start.x, start.y);
       graphic.lineTo(end.x, end.y);
     };
 
     const drawDottedLine = (
-      graphic: Graphics,
+      graphic: PIXI.Graphics,
       start: Vector2D,
       end: Vector2D,
       sections: number
@@ -106,7 +162,7 @@ export const SolarSystem = () => {
     };
 
     const drawRoad = (x: number, y: number, connections: number[]) => {
-      const road = new Graphics();
+      const road = new PIXI.Graphics();
 
       road.beginFill(0xd3d3d3);
       road.drawRect(ts1_3, ts1_3, ts2_3 - ts1_3, ts2_3 - ts1_3);
@@ -175,23 +231,37 @@ export const SolarSystem = () => {
       return road;
     };
 
+    const drawHouse = (x: number, y: number, orientation: string) => {
+      const house = new PIXI.Sprite(houseTextures[orientation]);
+      house.position.x = x;
+      house.position.y = y;
+      house.width = ts;
+      house.height = ts;
+      return house;
+    };
+
     const re = /(\w)(\d*)/;
     const houseTextures = {
-      '1': Texture.from('https://i.imgur.com/rGgNlV1.png'),
-      '2': Texture.from('https://i.imgur.com/OYDsDs7.png'),
-      '3': Texture.from('https://i.imgur.com/rGgNlV1.png'),
-      '4': Texture.from('https://i.imgur.com/jYSXUk0.png'),
+      '1': PIXI.Texture.from('https://i.imgur.com/rGgNlV1.png'),
+      '2': PIXI.Texture.from('https://i.imgur.com/OYDsDs7.png'),
+      '3': PIXI.Texture.from('https://i.imgur.com/rGgNlV1.png'),
+      '4': PIXI.Texture.from('https://i.imgur.com/jYSXUk0.png'),
     };
-    const houseTexture = Texture.from('https://i.imgur.com/FLqAFqq.png');
+    const dinerTexture = PIXI.Texture.from('https://i.imgur.com/gPK9T8l.png');
 
-    const drawChunk = (p: number, q: number, tileConfig: TileConfig) => {
+    const drawChunk = (
+      p: number,
+      q: number,
+      container: PIXI.Container,
+      tileConfig: TileConfig
+    ) => {
       tileConfig.forEach((tileRow, i) => {
         tileRow.forEach((tile, j) => {
           const occupant = drawEmptySquare(
             j * ts + p * 5 * ts,
             i * ts + q * 5 * ts
           );
-          app.stage.addChild(occupant);
+          container.addChild(occupant);
         });
       });
 
@@ -199,36 +269,218 @@ export const SolarSystem = () => {
         tileRow.forEach((tile, j) => {
           const match = re.exec(tile);
           if (match[1] === 'r') {
+            const road = { i: j + p * 5, j: i + q * 5 };
+            roads.push(road);
             const occupant = drawRoad(
-              j * ts + p * 5 * ts,
-              i * ts + q * 5 * ts,
+              road.i * ts,
+              road.j * ts,
               match[2].split('').map((i) => parseInt(i))
             );
-            app.stage.addChild(occupant);
+            container.addChild(occupant);
           } else if (match[1] === 'h') {
-            const house = new Sprite(houseTextures[match[2]]);
-            house.width = ts * 2 - 2;
-            house.height = ts * 2 - 2;
-            house.x = j * ts + p * 5 * ts + 2;
-            house.y = i * ts + q * 5 * ts + 2;
-            app.stage.addChild(house);
+            const house = { i: j + p * 5, j: i + q * 5 };
+            houses.push(house);
+            const occupant = drawHouse(house.i * ts, house.j * ts, match[2]);
+            container.addChild(occupant);
+          }
+        });
+      });
+
+      tileConfig.forEach((tileRow, i) => {
+        tileRow.forEach((tile, j) => {
+          const match = re.exec(tile);
+          if (match[1] === 'd') {
+            const diner = new PIXI.Sprite(dinerTexture);
+
+            const _diner = {
+              time: 0,
+              duration: 60,
+              prevPosition: {
+                x: 0,
+                y: 0,
+              },
+              nextPosition: {
+                x: 0,
+                y: 0,
+              },
+              update: () => {},
+            };
+
+            diner.width = ts - 2;
+            diner.height = ts - 2;
+            diner.x = j * ts + p * 5 * ts + 2;
+            diner.y = i * ts + q * 5 * ts + 2;
+            diner.name = 'diner1';
+            container.addChild(diner);
+
+            diner.interactive = true;
+            diner.buttonMode = true;
+            diner.on('pointerdown', () => {
+              _diner.time = 0;
+              _diner.prevPosition.x = diner.position.x;
+              _diner.prevPosition.y = diner.position.y;
+              let randomPosition = getRandomPosition();
+              for (let i = 0; i < 1000; i++) {
+                console.log(`next position: ${JSON.stringify(randomPosition)}`);
+                if (isValidPosition(randomPosition)) break;
+                randomPosition = getRandomPosition();
+              }
+              _diner.nextPosition.x = randomPosition.i * ts + 2;
+              _diner.nextPosition.y = randomPosition.j * ts + 2;
+              animations.push(() => {
+                _diner.time += 1;
+                if (_diner.time < _diner.duration) {
+                  diner.position.x =
+                    _diner.prevPosition.x +
+                    (_diner.nextPosition.x - _diner.prevPosition.x) *
+                      (_diner.time / _diner.duration);
+                  diner.position.y =
+                    _diner.prevPosition.y +
+                    (_diner.nextPosition.y - _diner.prevPosition.y) *
+                      (_diner.time / _diner.duration);
+                } else {
+                  console.log(
+                    `prev position: ${JSON.stringify(_diner.prevPosition)}`
+                  );
+                  console.log(
+                    `next position: ${JSON.stringify(_diner.nextPosition)}`
+                  );
+                  diner.x = _diner.nextPosition.x;
+                  diner.y = _diner.nextPosition.y;
+                  _diner.prevPosition = { ..._diner.nextPosition };
+                  animations.splice(0, 1);
+                }
+              });
+            });
           }
         });
       });
     };
 
-    drawChunk(0, 0, tileConfig);
-    drawChunk(1, 0, tileConfig2);
-    drawChunk(0, 1, tileConfig3);
-    drawChunk(1, 1, tileConfig3);
-    drawChunk(2, 1, tileConfig4);
+    const board = new PIXI.Container();
 
-    app.ticker.add(() => {
-      app.screen.height = gameElement.clientHeight;
-      app.screen.width = gameElement.clientWidth;
+    drawChunk(0, 0, board, tileConfig);
+    drawChunk(0, 1, board, tileConfig3);
+    drawChunk(1, 1, board, tileConfig3);
+    drawChunk(1, 0, board, tileConfig2);
+
+    console.log(JSON.stringify(roads));
+
+    board.pivot.x = board.width / 2;
+    board.pivot.y = board.height / 2;
+    board.position.x = app.screen.width / 2;
+    board.position.y = app.screen.height / 2;
+
+    app.stage.addChild(board);
+
+    const addDrawer = ({
+      app,
+      width,
+      height,
+      x,
+      y,
+      tabWidth,
+    }: {
+      app: PIXI.Application;
+      width: number;
+      height: number;
+      x: number;
+      y: number;
+      tabWidth: number;
+    }) => {
+      const drawerContainer = new PIXI.Container();
+
+      const drawerBody = new PIXI.Graphics();
+      drawerBody.lineStyle(2, 0x0, 1);
+      drawerBody.beginFill(0xeed9ba);
+      drawerBody.drawRoundedRect(0, 0, width, height, 20);
+      drawerBody.endFill();
+
+      // set recruit spacing
+      const rs = 0.1;
+      // set recruit tile size
+      const rts = 160;
+      for (let i = 0; i < 5; i++) {
+        for (let j = 0; j < 8; j++) {
+          const item = new PIXI.Graphics();
+          item.lineStyle(2, 0x0, 1);
+          item.beginFill(0xc370ff);
+          item.drawRoundedRect(
+            rts * ((1 + rs) * i + rs),
+            rts * ((1 + rs) * j + rs),
+            rts,
+            rts,
+            5
+          );
+          item.endFill();
+          drawerBody.addChild(item);
+        }
+      }
+
+      const drawerTab = new PIXI.Graphics();
+
+      drawerTab.lineStyle(2, 0x0, 1);
+      drawerTab.beginFill(0xeed9ba);
+      drawerTab.drawRoundedRect(-40, 0, 40, app.screen.height, 20);
+      drawerTab.endFill();
+
+      drawerContainer.addChild(drawerBody, drawerTab);
+
+      const drawer = {
+        open: false,
+      };
+
+      drawerContainer.x = drawer.open
+        ? app.screen.width - 1000
+        : app.screen.width;
+      drawerContainer.y = 0;
+
+      const translateDraw = () => {
+        drawer.open = !drawer.open;
+        drawerContainer.x = drawer.open
+          ? app.screen.width - 1000
+          : app.screen.width;
+      };
+
+      drawerTab.interactive = true;
+      drawerTab.buttonMode = true;
+      drawerTab.on('pointerdown', translateDraw);
+
+      app.stage.addChild(drawerContainer);
+    };
+
+    addDrawer({
+      app: app,
+      x: 0,
+      y: 0,
+      width: 1000,
+      height: app.screen.height,
+      tabWidth: 40,
     });
 
-    document.getElementById('game').appendChild(app.view);
+    const translate = (container, startPos, endPos, duration) => {
+      return {
+        startPos: startPos,
+        timeElapsed: 0,
+        render: (timeElapsed) => {
+          container.x = (endPos.x - startPos.x) * (duration / timeElapsed);
+          container.y = (endPos.y - startPos.y) * (duration / timeElapsed);
+        },
+      };
+    };
+
+    const updateAnimation = (delta, animation) => {
+      if (animation.timeElapsed < animation.duration) {
+        animation.timeElapsed += delta;
+        animation.render(delta, animation.timeElapsed);
+      }
+    };
+
+    app.ticker.add((delta) => {
+      animations.forEach((animation) => animation());
+    });
+
+    gameElement.appendChild(app.view);
 
     return () => app.destroy(true, true);
   }, []);
