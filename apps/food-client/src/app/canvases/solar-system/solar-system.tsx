33730a8f1a2809cd36b1169d8dc177e801b3f3e1
Tristan Dickson <tristandickson@gmail.com>
2021-12-02 15:16:58 +0000

    dynamic roads

--- apps/food-client/src/app/canvases/solar-system/solar-system.tsx
+++ apps/food-client/src/app/canvases/solar-system/solar-system.tsx
@@ -3,6 +3,27 @@ import { Application } from '@pixi/app';
 import { Graphics } from 'pixi.js';
 import { useEffect } from 'react';
 
+type Vector2D = {
+  x: number;
+  y: number;
+};
+
+type Tile = {
+  x: number;
+  y: number;
+  occupant?: any;
+};
+
+type TileConfig = string[][];
+
+type House = {
+  number: number;
+};
+
+type Road = {
+  connections: number[];
+};
+
 export const SolarSystem = () => {
   useEffect(() => {
     const gameElement = document.getElementById('game');
@@ -12,19 +33,32 @@ export const SolarSystem = () => {
     });
     app.stage.sortableChildren = true;
 
+    const ts = 90;
+    const ts1_3 = ts / 3;
+    const ts1_2 = ts / 2;
+    const ts2_3 = (2 * ts) / 3;
+
     const tileConfig = [
-      ['es', 'es', 'rv', 'ho', 'ho'],
-      ['es', 'es', 'rv', 'ho', 'ho'],
-      ['rh', 'rh', 'rc', 'rh', 'rh'],
-      ['es', 'es', 'rv', 'es', 'es'],
-      ['es', 'es', 'rv', 'es', 'es'],
+      ['e', 'e', 'r24', 'h', 'h'],
+      ['e', 'e', 'r24', 'h', 'h'],
+      ['r13', 'r13', 'r1234', 'r13', 'r13'],
+      ['e', 'e', 'r24', 'e', 'e'],
+      ['e', 'e', 'r24', 'e', 'e'],
     ];
 
-    const drawEmptySquare = (x, y) => {
+    const tileConfig2 = [
+      ['e', 'e', 'r24', 'e', 'e'],
+      ['e', 'e', 'r24', 'e', 'e'],
+      ['r13', 'r13', 'r124', 'e', 'e'],
+      ['e', 'e', 'r24', 'e', 'e'],
+      ['e', 'e', 'r24', 'e', 'e'],
+    ];
+
+    const drawEmptySquare = (x: number, y: number) => {
       const square = new Graphics();
       square.lineStyle(2, 0x0, 1);
       square.beginFill(0xffffff);
-      square.drawRect(2, 2, 78, 78);
+      square.drawRect(2, 2, ts - 2, ts - 2);
       square.endFill();
 
       square.position.x = x;
@@ -33,105 +67,132 @@ export const SolarSystem = () => {
       return square;
     };
 
-    const drawRoad = (x, y, rotation) => {
+    const DrawLine = (graphic: Graphics, start: Vector2D, end: Vector2D) => {
+      graphic.moveTo(start.x, start.y);
+      graphic.lineTo(end.x, end.y);
+    };
+
+    const DrawDottedLine = (
+      graphic: Graphics,
+      start: Vector2D,
+      end: Vector2D,
+      sections: number
+    ) => {
+      const xStep = (end.x - start.x) / (sections * 4 + 1);
+      const yStep = (end.y - start.y) / (sections * 4 + 1);
+      console.log(xStep);
+      graphic.moveTo(start.x, start.y);
+      for (let i = 0; i < sections * 2 + 1; i += 2) {
+        graphic.lineTo(start.x + xStep * 2 * i, start.y + yStep * 2 * i);
+        graphic.moveTo(
+          start.x + xStep * 2 * (i + 1),
+          start.y + yStep * 2 * (i + 1)
+        );
+      }
+    };
+
+    const drawRoad = (x: number, y: number, connections: number[]) => {
       const road = new Graphics();
 
       road.beginFill(0xd3d3d3);
-      road.drawRect(0, 0, 80, 80);
+      road.drawRect(ts1_3, ts1_3, ts1_3, ts1_3);
       road.endFill();
 
       road.lineStyle(4, 0x0, 1);
 
-      const lineX = 40;
-      road.moveTo(0, lineX);
-      const lineSections = 4;
-      const lineLength = 78;
-      const sectionLength = lineLength / (lineSections * 2);
-      for (let i = 0; i < 10; i += 2) {
-        road.lineTo(i * sectionLength, lineX);
-        road.moveTo((i + 1) * sectionLength, lineX);
+      console.log(connections);
+
+      if (connections.includes(1)) {
+        road.lineStyle(4, 0x0, 0);
+        road.beginFill(0xd3d3d3);
+        road.drawRect(0, ts1_3, ts1_3, ts1_3);
+        road.endFill();
+
+        road.lineStyle(4, 0x0, 1);
+        DrawLine(road, { x: 0, y: ts1_3 }, { x: ts1_3, y: ts1_3 });
+        DrawDottedLine(road, { x: 0, y: ts1_2 }, { x: ts1_2, y: ts1_2 }, 2);
+        DrawLine(road, { x: 0, y: ts2_3 }, { x: ts1_3, y: ts2_3 });
+      } else {
+        DrawLine(road, { x: ts1_3, y: ts1_3 }, { x: ts1_3, y: ts2_3 });
+      }
+
+      if (connections.includes(2)) {
+        road.lineStyle(4, 0x0, 0);
+        road.beginFill(0xd3d3d3);
+        road.drawRect(ts1_3, 0, ts1_3, ts1_3);
+        road.endFill();
+
+        road.lineStyle(4, 0x0, 1);
+        DrawLine(road, { x: ts1_3, y: ts1_3 }, { x: ts1_3, y: 0 });
+        DrawDottedLine(road, { x: ts1_2, y: 0 }, { x: ts1_2, y: ts1_2 }, 2);
+        DrawLine(road, { x: ts2_3, y: ts1_3 }, { x: ts2_3, y: 0 });
+      } else {
+        DrawLine(road, { x: ts1_3, y: ts1_3 }, { x: ts2_3, y: ts1_3 });
+      }
+
+      if (connections.includes(3)) {
+        road.lineStyle(4, 0x0, 0);
+        road.beginFill(0xd3d3d3);
+        road.drawRect(ts2_3, ts1_3, ts1_3, ts1_3);
+        road.endFill();
+
+        road.lineStyle(4, 0x0, 1);
+        DrawLine(road, { x: ts2_3, y: ts1_3 }, { x: ts, y: ts1_3 });
+        DrawDottedLine(road, { x: ts, y: ts1_2 }, { x: ts1_2, y: ts1_2 }, 2);
+        DrawLine(road, { x: ts2_3, y: ts2_3 }, { x: ts, y: ts2_3 });
+      } else {
+        DrawLine(road, { x: ts2_3, y: ts1_3 }, { x: ts2_3, y: ts2_3 });
+      }
+
+      if (connections.includes(4)) {
+        road.lineStyle(4, 0x0, 0);
+        road.beginFill(0xd3d3d3);
+        road.drawRect(ts1_3, ts2_3, ts1_3, ts1_3);
+        road.endFill();
+
+        road.lineStyle(4, 0x0, 1);
+        DrawLine(road, { x: ts1_3, y: ts2_3 }, { x: ts1_3, y: ts });
+        DrawDottedLine(road, { x: ts1_2, y: ts }, { x: ts1_2, y: ts1_2 }, 2);
+        DrawLine(road, { x: ts2_3, y: ts2_3 }, { x: ts2_3, y: ts });
+      } else {
+        DrawLine(road, { x: ts2_3, y: ts2_3 }, { x: ts1_3, y: ts2_3 });
       }
 
       road.position.x = x;
       road.position.y = y;
-      if (rotation > 0) road.position.x += 80;
-      road.rotation = rotation;
       return road;
     };
 
-    const drawCrossRoad = (x, y) => {
-      const road = new Graphics();
-      road.beginFill(0xd3d3d3);
-      road.drawRect(0, 0, 80, 80);
-      road.endFill();
-      road.lineStyle(4, 0x0, 1);
+    const re = /(\w)(\d*)/;
 
-      const lineSections = 4;
-      const lineLength = 70;
-      const sectionLength = lineLength / (lineSections * 2);
-      const lineX = 40;
-      road.moveTo(0, lineX);
-      for (let i = 0; i < 10; i += 2) {
-        road.lineTo(i * sectionLength, lineX);
-        road.moveTo((i + 1) * sectionLength, lineX);
-      }
-
-      road.moveTo(lineX, 0);
-      for (let i = 0; i < 10; i += 2) {
-        road.lineTo(lineX, i * sectionLength);
-        road.moveTo(lineX, (i + 1) * sectionLength);
-      }
-
-      road.position.x = x;
-      road.position.y = y;
-      return road;
-    };
-
-    // tileConfig.forEach((tileRow) => {
-    //   tileRow.map((tile) => {
-    //     if (tile === 'es') {
-    //       return {
-    //         label: 'Empty Square',
-    //         drawn: '',
-    //       };
-    //     } else if (tile === 'rh') {
-    //       return {
-    //         label: 'Empty Square',
-    //         drawn: '',
-    //       };
-    //     } else if (tile === 'rv') {
-    //       return {
-    //         label: 'Empty Square',
-    //         drawn: '',
-    //       };
-    //     } else if (tile === 'rc') {
-    //       return {
-    //         label: 'Empty Square',
-    //         drawn: '',
-    //       };
-    //     }
-    //   });
-    // });
-
-    tileConfig.forEach((tileRow, i) => {
-      tileRow.forEach((tile, j) => {
-        let occupant;
-        if (tile === 'es') {
-          occupant = drawEmptySquare(j * 80, i * 80);
+    const drawChunk = (p: number, q: number, tileConfig: TileConfig) => {
+      tileConfig.forEach((tileRow, i) => {
+        tileRow.forEach((tile, j) => {
+          const occupant = drawEmptySquare(
+            j * ts + p * 5 * ts,
+            i * ts + q * 5 * ts
+          );
           app.stage.addChild(occupant);
-        } else if (tile === 'rh') {
-          occupant = drawRoad(j * 80, i * 80, 0);
-          app.stage.addChild(occupant);
-        } else if (tile === 'rv') {
-          occupant = drawRoad(j * 80, i * 80, Math.PI / 2);
-          app.stage.addChild(occupant);
-        } else if (tile === 'rc') {
-          occupant = drawCrossRoad(j * 80, i * 80);
-          app.stage.addChild(occupant);
-        }
-        // app.stage.addChild(occupant);
+        });
       });
-    });
+
+      tileConfig.forEach((tileRow, i) => {
+        tileRow.forEach((tile, j) => {
+          const match = re.exec(tile);
+          if (match[1] === 'r') {
+            const occupant = drawRoad(
+              j * ts + p * 5 * ts,
+              i * ts + q * 5 * ts,
+              match[2].split('').map((i) => parseInt(i))
+            );
+            app.stage.addChild(occupant);
+          }
+        });
+      });
+    };
+
+    drawChunk(0, 0, tileConfig);
+    drawChunk(1, 0, tileConfig2);
 
     app.ticker.add(() => {
       app.screen.height = gameElement.clientHeight;
