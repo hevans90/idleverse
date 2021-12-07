53027aa944181a9b846e926658c80814b24e6126
Tristan Dickson <tristandickson@gmail.com>
2021-12-07 16:14:29 +0000

    hasura food-db init

---
+++ hasura-local/food-db/migrations/food_db/1638893006400_player table/down.sql
@@ -0,0 +1,10 @@
+
+
+
+DROP TABLE "food_schema"."player";
+
+drop schema "food_schema" cascade;
+
+DROP TABLE "food_schema"."player";
+
+drop schema "food_schema" cascade;
---
+++ hasura-local/food-db/migrations/food_db/1638893006400_player table/up.sql
@@ -0,0 +1,10 @@
+
+create schema "food_schema";
+
+
+CREATE TABLE "food_schema"."player" ("id" uuid NOT NULL, "name" text NOT NULL, PRIMARY KEY ("id") );
+
+
+create schema "food_schema";
+
+CREATE TABLE "food_schema"."player" ("id" uuid NOT NULL, "name" text NOT NULL, PRIMARY KEY ("id") );
