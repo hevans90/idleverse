76e3a96b6e4f0ac44a726e5bc4b6bed3ef2c7986
Tristan Dickson <tristandickson@gmail.com>
2021-12-12 15:02:08 +0000

    added foreign keys and some new tables

--- apply-latest.sh
+++ apply-latest.sh
@@ -1,4 +1,3 @@
 cd hasura-local/food-db
-npx hasura migrate create \"$1\" --from-server --database-name default
 ver=$(npx hasura migrate status --database-name default | egrep -o '[0-9]{13}' | tail -1)
 npx hasura migrate apply --version "$ver" --skip-execution --database-name default
