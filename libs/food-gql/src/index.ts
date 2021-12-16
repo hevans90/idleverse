876069af942278bd6bf90ca4f578f1708f6414ec
Tristan Dickson <tristandickson@gmail.com>
2021-12-16 15:46:36 +0000

    simplified directory structure

--- libs/food-gql/src/index.ts
+++ libs/food-gql/src/index.ts
@@ -1,2 +1,2 @@
-export * from './lib/food/food-api';
+export * from './lib/food-api';
 export * from './lib/apollo-bootstrapper';
--- libs/galaxy-gql/src/index.ts
+++ libs/galaxy-gql/src/index.ts
@@ -1,2 +1,2 @@
-export * from './lib/idleverse/galaxy-api';
+export * from './lib/galaxy-api';
 export * from './lib/apollo-bootstrapper';
