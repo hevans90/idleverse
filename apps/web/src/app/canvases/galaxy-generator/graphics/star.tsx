5fa8074e8ff6a4509ad904a94f80dca4fa42b16b
hevans90 <hevans9000@gmail.com>
2021-12-24 19:03:23 -0300

    fix(galaxy-viewer): positioning error with radii

--- apps/web/src/app/canvases/galaxy-generator/graphics/star.tsx
+++ apps/web/src/app/canvases/galaxy-generator/graphics/star.tsx
@@ -10,36 +10,42 @@ export type StarProps = {
 };
 
 const claimedCol = 0xff0000;
+const claimedRadius = 4;
 const unclaimedCol = 0xffffff;
+const unclaimedRadius = 2;
 
 export const Star = ({ x, y, isClaimed, id }: Partial<StarProps>) => {
+  const r = isClaimed ? claimedRadius : unclaimedRadius;
+
   const g = new PIXI.Graphics()
     .clear()
     .beginFill(isClaimed ? claimedCol : unclaimedCol)
-    .drawCircle(x, y, isClaimed ? 5 : 1)
+    .drawCircle(r / 2, r / 2, r)
     .endFill();
 
   g.name = id;
+  g.x = x;
+  g.y = y;
 
   return g;
 };
 
 export const claimStar = (
-  { x, y, id }: ReturnType<typeof getCelestialPositionAndId>,
+  { id }: ReturnType<typeof getCelestialPositionAndId>,
   container: Container
 ) =>
   (container.getChildByName(id) as PIXI.Graphics)
     .clear()
     .beginFill(claimedCol)
-    .drawCircle(x, y, 3)
+    .drawCircle(claimedRadius / 2, claimedRadius / 2, claimedRadius)
     .endFill();
 
 export const unclaimStar = (
-  { x, y, id }: ReturnType<typeof getCelestialPositionAndId>,
+  { id }: ReturnType<typeof getCelestialPositionAndId>,
   container: Container
 ) =>
   (container.getChildByName(id) as PIXI.Graphics)
     .clear()
     .beginFill(unclaimedCol)
-    .drawCircle(x, y, 1)
+    .drawCircle(unclaimedRadius / 2, unclaimedRadius / 2, unclaimedRadius)
     .endFill();
