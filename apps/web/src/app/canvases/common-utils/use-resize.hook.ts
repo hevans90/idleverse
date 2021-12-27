a52f06cab4e99481ac71415dd507a9f164e95400
hevans90 <hevans9000@gmail.com>
2021-12-27 12:31:15 -0300

    fix: resize hook

--- apps/web/src/app/canvases/common-utils/use-resize.hook.ts
+++ apps/web/src/app/canvases/common-utils/use-resize.hook.ts
@@ -1,9 +1,11 @@
+import { useReactiveVar } from '@apollo/client';
 import { useEffect, useState } from 'react';
 import { sideNavWidth, topBarHeight } from '../../components/layout';
+import { layoutVar } from '../../_state/persisted-reactive-variables';
 import { generatorControlsHeight } from '../galaxy-generator/ui/generator-controls';
 
-const getScreenSize = (controlsShowing = false) => ({
-  width: window.innerWidth - sideNavWidth,
+const getScreenSize = (controlsShowing = false, sideNav: boolean) => ({
+  width: window.innerWidth - (sideNav ? sideNavWidth : 0),
   height:
     window.innerHeight -
     topBarHeight -
@@ -11,13 +13,23 @@ const getScreenSize = (controlsShowing = false) => ({
 });
 
 export const useResize = (galaxyGen = false) => {
-  const [screenSize, setScreenSize] = useState(getScreenSize(galaxyGen));
+  const { sideNav } = useReactiveVar(layoutVar);
+
+  const [screenSize, setScreenSize] = useState(
+    getScreenSize(galaxyGen, sideNav)
+  );
 
   useEffect(() => {
-    const onResize = () => setScreenSize(getScreenSize(galaxyGen));
+    const onResize = () => setScreenSize(getScreenSize(galaxyGen, sideNav));
     window.addEventListener('resize', onResize);
     return () => window.removeEventListener('resize', onResize);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [galaxyGen]);
+
+  useEffect(() => {
+    setScreenSize(getScreenSize(galaxyGen, sideNav));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [sideNav]);
 
   return screenSize;
 };
