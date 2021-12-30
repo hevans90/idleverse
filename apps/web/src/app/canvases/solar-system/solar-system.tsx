a59cc50146f024efd0d55fb1000d5a478939b86a
hevans90 <hevans9000@gmail.com>
2021-12-30 10:54:15 -0300

    refactor(solar-system): remove bloat; improve readability

--- apps/web/src/app/canvases/solar-system/solar-system.tsx
+++ apps/web/src/app/canvases/solar-system/solar-system.tsx
@@ -1,5 +1,4 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useReactiveVar } from '@apollo/client';
 import { useApp } from '@inlet/react-pixi';
 import { Container } from 'pixi.js';
 import { useEffect, useRef } from 'react';
@@ -33,18 +32,10 @@ export const SolarSystem = () => {
 
   useViewport(app, size, solarSystemContainerRef, true);
 
-  let { simulationSpeed, viewAngle, viewDistance } =
-    useReactiveVar(solarSystemConfigVar);
-
-  useEffect(() => {
-    viewAngle = (viewAngle * Math.PI) / 180;
-  }, [viewAngle]);
-
   useEffect(() => {
     solarSystemContainerRef.current.x = size.width / 2;
     solarSystemContainerRef.current.y = size.height / 2;
 
-    viewAngle = (viewAngle * Math.PI) / 180;
     solarSystemContainerRef.current.sortableChildren = true;
 
     const systemOrigin = { x: 0, y: 0 };
@@ -58,7 +49,7 @@ export const SolarSystem = () => {
       sunSpriteConfig
     );
     const sunConfig: PlanetConfig = {
-      origin: { x: 400, y: 400 },
+      origin: { x: systemOrigin.x, y: systemOrigin.y },
       orbit: { x: 0, y: 0, speed: 1 },
     };
     const sun: Planet = CreatePlanet({
@@ -69,7 +60,7 @@ export const SolarSystem = () => {
 
     const earthConfig: PlanetConfig = {
       origin: { x: 0, y: 0 },
-      orbit: { x: 250, y: 150, speed: 1 },
+      orbit: { x: 200, y: 200, speed: 1 },
     };
     const earthSprite = CreateAnimatedPlanetSprite(
       solarSystemContainerRef.current,
@@ -114,18 +105,11 @@ export const SolarSystem = () => {
 
     const planets = [sun, earth, mars, moon];
 
-    // planets.forEach((planet) =>
-    //   UpdatePlanetPosition(timeVar(), planet, simulationSpeed)
-    // );
-    // planets.forEach((planet) =>
-    //   DrawPlanet(planet, systemOrigin, viewDistance, viewAngle)
-    // );
-    sun.config.origin = systemOrigin;
-
     app.ticker.add(() => {
-      viewAngle = (solarSystemConfigVar().viewAngle * Math.PI) / 180;
-      viewDistance = solarSystemConfigVar().viewDistance;
-      simulationSpeed = solarSystemConfigVar().simulationSpeed;
+      // eslint-disable-next-line prefer-const
+      let { viewAngle, viewDistance, simulationSpeed } = solarSystemConfigVar();
+
+      viewAngle = (viewAngle * Math.PI) / 180;
 
       planets.forEach((planet) =>
         UpdatePlanetPosition(timeVar(), planet, simulationSpeed)
@@ -134,11 +118,6 @@ export const SolarSystem = () => {
         DrawPlanet(planet, systemOrigin, viewDistance, viewAngle)
       );
     });
-    // setTimeout(() => {}, 100);
-
-    // return () => {
-    //   timeVar(0);
-    // };
   }, []);
 
   // eslint-disable-next-line react/jsx-no-useless-fragment
