2dc434f877650b5669334ad94ad76823a2cd0540
hevans90 <hevans9000@gmail.com>
2022-04-03 16:27:10 +0100

    fix: tests

--- apps/idleverse/web/src/app/canvases/_utils/theme-colour-conversions.spec.ts
+++ apps/idleverse/web/src/app/canvases/_utils/theme-colour-conversions.spec.ts
@@ -1,14 +1,14 @@
-import { themeColToRGB } from './theme-colour-conversions';
+import { hexToRGB } from './theme-colour-conversions';
 
-describe('themeColToRGB', () => {
+describe('hexToRGB', () => {
   it('should successfully convert some preset hex values', () => {
-    expect(themeColToRGB('#F4F1F2')).toEqual({
+    expect(hexToRGB('#F4F1F2')).toEqual({
       r: 244,
       g: 241,
       b: 242,
     });
 
-    expect(themeColToRGB('#4E1818')).toEqual({
+    expect(hexToRGB('#4E1818')).toEqual({
       r: 78,
       g: 24,
       b: 24,
