616a71f43a359bfedf46d64571e24cc4bc6037e5
hevans90 <hevans9000@gmail.com>
2022-04-20 20:56:14 +0100

    build: add docker codegen

---
+++ codegen.docker.js
@@ -0,0 +1,28 @@
+module.exports = {
+  overwrite: true,
+  generates: {
+    'libs/galaxy-gql/src/lib/galaxy-api.ts': {
+      documents: 'libs/galaxy-gql/src/lib/**/*.graphql',
+      schema: [
+        {
+          'http://localhost:8080/v1/graphql': {
+            headers: {
+              'x-hasura-admin-secret': 'myadminsecretkey',
+            },
+          },
+        },
+      ],
+      plugins: [
+        'typescript',
+        'typescript-operations',
+        'typescript-react-apollo',
+      ],
+      config: {
+        skipTypename: false,
+        withHooks: false,
+        withHOC: false,
+        withComponent: false,
+      },
+    },
+  },
+};
