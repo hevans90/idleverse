3d09e268c52adedf2feba2e5f576be172af9bd12
hevans90 <hevans9000@gmail.com>
2022-04-20 16:02:09 +0100

    feat(server): planet creation passthrough

--- apps/idleverse/game-server/src/entities/celestial-management.ts
+++ apps/idleverse/game-server/src/entities/celestial-management.ts
@@ -1,36 +1,34 @@
-import {
-  Planet,
-  TryInsertPlanetMutationVariables,
-} from '@idleverse/galaxy-gql';
-import { Arg, Ctx, Field, Mutation, ObjectType, Resolver } from 'type-graphql';
+import 'reflect-metadata';
+import { Arg, Authorized, Ctx, Mutation, Resolver } from 'type-graphql';
 import { Context } from '../datasources/context';
-
-@ObjectType()
-class CelestialManagement {
-  @Field()
-  createdPlanet: Pick<Planet, '__typename' | 'id' | 'name' | 'owner_id'>;
-}
+import {
+  CelestialManagement,
+  PartialPlanet,
+  PlanetCreationInput,
+} from './celestial-types';
 
 @Resolver((of) => CelestialManagement)
 export class CelestialManagementResolver {
+  @Authorized('user')
   @Mutation((returns) => CelestialManagement, { nullable: true })
   async createPlanet(
     @Ctx() context: Context,
-    @Arg('planet_input') variables: TryInsertPlanetMutationVariables
-  ) {
+    @Arg('input') input: PlanetCreationInput
+  ): Promise<CelestialManagement> {
     if (!context.id) throw new Error('User id not in token');
 
     const data = (
-      await context.dataSources.hasuraAPI.tryInsertPlanetToCelestial(variables)
+      await context.dataSources.hasuraAPI.tryInsertPlanetToCelestial({ input })
     ).data;
 
-    const planet = data?.insert_planet_one;
+    const planet: PartialPlanet = data?.insert_planet_one;
 
-    if (planet) {
-      const celestialManagement = new CelestialManagement();
-      celestialManagement.createdPlanet = planet;
-      return celestialManagement;
+    if (!planet) {
+      throw new Error('Failed to insert planet');
     }
-    throw new Error('Failed to insert planet');
+
+    const celestialManagement = new CelestialManagement();
+    celestialManagement.createdPlanet = planet;
+    return celestialManagement;
   }
 }
