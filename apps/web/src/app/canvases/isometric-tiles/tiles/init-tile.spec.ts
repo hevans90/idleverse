30bdf6e8a8a0432a318867fe2106ad15b80ee009
hevans90 <hevans9000@gmail.com>
2022-07-16 16:50:42 +0100

    test(isometric): remove scale

--- apps/web/src/app/canvases/isometric-tiles/tiles/init-tile.spec.ts
+++ apps/web/src/app/canvases/isometric-tiles/tiles/init-tile.spec.ts
@@ -12,20 +12,19 @@ describe('initTile', () => {
   });
 
   let tile: IsometricGraphic;
-  const { scale } = config;
 
   beforeEach(() => (tile = initTile(0, 1, config)));
 
   it('should set corner coords successfully', () => {
-    expect(tile.c1).toEqual([11, 6].map((i) => i * scale));
-    expect(tile.c2).toEqual([10, 6.5].map((i) => i * scale));
-    expect(tile.c3).toEqual([9, 6].map((i) => i * scale));
-    expect(tile.c4).toEqual([10, 5.5].map((i) => i * scale));
+    expect(tile.c1).toEqual([11, 6].map((i) => i));
+    expect(tile.c2).toEqual([10, 6.5].map((i) => i));
+    expect(tile.c3).toEqual([9, 6].map((i) => i));
+    expect(tile.c4).toEqual([10, 5.5].map((i) => i));
   });
 
   it('should set hit area correctly', () => {
     expect(tile.hitArea).toEqual(
-      new PIXI.Polygon([11, 6, 10, 6.5, 9, 6, 10, 5.5].map((i) => i * scale))
+      new PIXI.Polygon([11, 6, 10, 6.5, 9, 6, 10, 5.5].map((i) => i))
     );
   });
 });
