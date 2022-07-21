46cf9ce41e5e8fbfeebfe35f340e00aeecf0968c
hevans90 <hevans9000@gmail.com>
2022-07-21 12:17:08 +0100

    feat(web): generic pixi wrapper

---
+++ apps/web/src/app/canvases/_utils/pixi-wrapper.tsx
@@ -0,0 +1,32 @@
+import { Box, Theme, useTheme } from '@chakra-ui/react';
+import { hexStringToNumber } from '@idleverse/theme';
+import { Stage } from '@inlet/react-pixi';
+import { useDisableWheelZoom } from './use-disable-wheel-zoom.hook';
+import { controls, useResize } from './use-resize.hook';
+
+export const PixiWrapper = (props: {
+  children?: JSX.Element;
+  ui?: JSX.Element;
+  resizeControls?: controls;
+}) => {
+  const { disableZoomCallback } = useDisableWheelZoom();
+
+  const { colors } = useTheme<Theme>();
+
+  const size = useResize(props.resizeControls || 'none');
+
+  return (
+    <Box position="relative" ref={disableZoomCallback}>
+      <Stage
+        {...size}
+        options={{
+          backgroundColor: hexStringToNumber(colors.gray['800']),
+          antialias: true,
+        }}
+      >
+        {props?.children}
+      </Stage>
+      {props?.ui}
+    </Box>
+  );
+};
