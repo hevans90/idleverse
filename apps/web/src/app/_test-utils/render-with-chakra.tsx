95fec44875e2c35411cba1f32631574f5c8eabd6
hevans90 <hevans9000@gmail.com>
2022-08-09 12:34:25 +0100

    test: update snapshots; fix theme util

--- apps/web/src/app/_test-utils/render-with-chakra.tsx
+++ apps/web/src/app/_test-utils/render-with-chakra.tsx
@@ -4,7 +4,9 @@ import { render } from '@testing-library/react';
 
 export const renderWithChakra = (ui) => {
   const Wrapper = ({ children }) => (
-    <ChakraProvider theme={theme}>{children}</ChakraProvider>
+    <ChakraProvider theme={theme({ primary: 'gray', secondary: 'teal' })}>
+      {children}
+    </ChakraProvider>
   );
 
   return render(ui, { wrapper: Wrapper });
