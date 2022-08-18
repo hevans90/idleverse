81a6479aa15884cc583f4a75352812f398c39a32
hevans90 <hevans9000@gmail.com>
2022-08-18 20:02:12 +0100

    fix(hasura): initial quest step migrations

---
+++ apps/hasura/db/migrations/default/1660848380712_set_initial_quest_steps/up.sql
@@ -0,0 +1,7 @@
+UPDATE quest_step
+SET initial = true
+WHERE id = '33c7a786-7d99-4376-8c9f-6ec1944d002e';
+
+UPDATE quest_step
+SET initial = true
+WHERE id = 'df989c66-0f6b-4360-831b-c01cfcbac849';
---
+++ apps/hasura/db/migrations/default/1660848416866_alter_table_public_quest_step_alter_column_initial/up.sql
@@ -0,0 +1,1 @@
+alter table "public"."quest_step" alter column "initial" set not null;
