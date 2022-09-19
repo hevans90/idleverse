8fdac48f0367cf76c25b33c29024c2cd287a2243
hevans90 <hevans9000@gmail.com>
2022-09-19 15:26:37 +0100

    feat(hasura): add image urls to resources

---
+++ apps/hasura/db/migrations/default/1663596715167_alter_table_public_resource_type_add_column_image_url/down.sql
@@ -0,0 +1,4 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- alter table "public"."resource_type" add column "image_url" text
+--  null;
