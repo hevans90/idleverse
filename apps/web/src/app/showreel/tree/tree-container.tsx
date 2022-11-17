6efc2a65abf0d2f15797e187108306ef05e4b3ca
hevans90 <hevans9000@gmail.com>
2022-11-17 16:14:31 +0000

    refactor(tree): rename ui

--- apps/web/src/app/showreel/tree/tree-container.tsx
+++ apps/web/src/app/showreel/tree/tree-container.tsx
@@ -2,8 +2,8 @@ import { useReactiveVar } from '@apollo/client';
 import { PixiWrapper } from '../../canvases/_utils/pixi-wrapper';
 import { technologiesVar } from '../../_state/technologies';
 import { ResearchTree } from './tree';
-import { TreeSearch } from './ui/search';
-import { TreeSettings } from './ui/settings';
+import { TreeSearch } from './ui/tree-search';
+import { TreeSettings } from './ui/tree-settings';
 
 export const TreeContainer = () => {
   const technologies = useReactiveVar(technologiesVar);
