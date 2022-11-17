8006d9e5c1fa5ba71f4fa70b1799813e50d837ae
hevans90 <hevans9000@gmail.com>
2022-11-17 15:48:59 +0000

    feat(web): dev auth badge

--- apps/web/src/app/_auth/auth.tsx
+++ apps/web/src/app/_auth/auth.tsx
@@ -1,11 +1,15 @@
+import { useReactiveVar } from '@apollo/client';
 import { useAuth0 } from '@auth0/auth0-react';
-import { Avatar, Box, Text } from '@chakra-ui/react';
+import { Avatar, Badge, Box, Text } from '@chakra-ui/react';
 import { Loading } from '../components/loading';
+import { roleVar } from '../_state/reactive-variables';
 import { LoginButton } from './login-button';
 import { LogoutButton } from './logout-button';
 
 export const Auth = () => {
   const { user, isAuthenticated, isLoading } = useAuth0();
+
+  const role = useReactiveVar(roleVar);
 
   if (isLoading) {
     return <Loading fontSize="md"></Loading>;
@@ -19,6 +23,7 @@ export const Auth = () => {
         <Text marginRight="1rem" display={['none', 'none', 'block']}>
           {user.name}
         </Text>
+        {role === 'dev' && <Badge mr={3}>dev</Badge>}
 
         <LogoutButton></LogoutButton>
       </Box>
