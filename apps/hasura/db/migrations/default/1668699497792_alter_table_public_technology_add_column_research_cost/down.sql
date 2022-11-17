38efc7d4d1f237102c98aba4364efdf371b8537e
hevans90 <hevans9000@gmail.com>
2022-11-17 15:40:55 +0000

    feat(tree): extend data model

---
+++ apps/hasura/db/migrations/default/1668699497792_alter_table_public_technology_add_column_research_cost/down.sql
@@ -0,0 +1,4 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- alter table "public"."technology" add column "research_cost" integer
+--  not null default '0';
---
+++ apps/hasura/db/migrations/default/1668699497792_alter_table_public_technology_add_column_research_cost/up.sql
@@ -0,0 +1,2 @@
+alter table "public"."technology" add column "research_cost" integer
+ not null default '0';
