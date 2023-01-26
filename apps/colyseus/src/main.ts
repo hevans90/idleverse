3ee1755d42c05ff766708f591a1fe8197a71d03f
hevans90 <hevans9000@gmail.com>
2023-01-26 16:41:33 +0000

    fix(colyseus): revent to classic websockets

--- apps/colyseus/src/main.ts
+++ apps/colyseus/src/main.ts
@@ -1,25 +1,19 @@
 import cors from 'cors';
 import express from 'express';
-import expressify from 'uwebsockets-express';
+import { createServer } from 'http';
 
 import { monitor } from '@colyseus/monitor';
-import { uWebSocketsTransport } from '@colyseus/uwebsockets-transport';
-
 import { Server } from 'colyseus';
 import { GameRoom } from './room/room';
 
 const port = Number(process.env.PORT || 1447);
-
-const transport = new uWebSocketsTransport();
-
-const app = expressify(transport.app);
+const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-const colyseusServer = new Server({
-  transport,
-});
+const server = createServer(app);
+const colyseusServer = new Server({ server });
 
 // register room handlers
 colyseusServer.define('my-room', GameRoom, { name: 'Test', mald: 'nice' });
