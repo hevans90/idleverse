721a64c90142bf75f2bf06809dacd48ebfecbe67
hevans90 <hevans9000@gmail.com>
2023-02-08 11:13:21 +0000

    fix(web): ignore keypress ctrl/meta combos for now

--- apps/web/src/app/hooks/use-keypress.ts
+++ apps/web/src/app/hooks/use-keypress.ts
@@ -5,6 +5,11 @@ export const useKeypress = (key: string, handler: () => unknown) => {
 
   useEffect(() => {
     eventListenerRef.current = (event: KeyboardEvent) => {
+      if (event.metaKey || event.ctrlKey) {
+        // ignore combinations for now
+        return;
+      }
+
       if (event.code === key) {
         // don't fire custom keydown event handlers for inputs... or users can't type in chat etc.
         if ((event.target as HTMLElement)?.nodeName === 'INPUT') {
