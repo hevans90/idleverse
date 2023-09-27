23b245e3993ada042b06168e67ff38a8c0cc4420
Harri Evans <hevans9000@gmail.com>
2023-09-27 17:28:28 +0100

    feat(hasura): update test data & generator costs

---
+++ apps/hasura/db/migrations/default/1695807587164_change_mine_cost_to_common_metals/down.sql
@@ -0,0 +1,5 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- UPDATE resource_generator
+-- SET cost_resource_type_id_1 = '21b6f220-4f66-4454-a08c-f98e8a25f72b'
+-- WHERE id = 'b998cb81-36e8-4788-82c2-023ee19b8c2a';
---
+++ apps/hasura/db/migrations/default/1695831542663_updat_test_celestial_empire_id/down.sql
@@ -0,0 +1,5 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- UPDATE celestial
+-- SET galactic_empire_id = 'fee29a04-436d-4296-adaa-468d38269413'
+-- WHERE id = 'b741230228a6404323d9f89c4435cd9305345afd';
