91f1d5c538703bfa65e22b7d8ee081bf337c0a83
Harri Evans <hevans9000@gmail.com>
2024-02-03 18:34:50 +0000

    feat(minio): deployment script made generic

--- apps/minio/seed/seed.ts
+++ apps/minio/seed/seed.ts
@@ -1,7 +1,7 @@
 import { readdir } from 'fs/promises';
-import { BucketItem, Client } from 'minio';
+import { BucketItem, Client, ClientOptions } from 'minio';
 
-const publicPolicy = JSON.stringify({
+const publicPolicyFactory = (bucketName: string) => ({
   Version: '2012-10-17',
   Statement: [
     {
@@ -12,7 +12,7 @@ const publicPolicy = JSON.stringify({
         's3:ListBucket',
         's3:ListBucketMultipartUploads',
       ],
-      Resource: ['arn:aws:s3:::music'],
+      Resource: [`arn:aws:s3:::${bucketName}`],
     },
     {
       Effect: 'Allow',
@@ -24,39 +24,53 @@ const publicPolicy = JSON.stringify({
         's3:PutObject',
         's3:AbortMultipartUpload',
       ],
-      Resource: ['arn:aws:s3:::music/*'],
+      Resource: [`arn:aws:s3:::${bucketName}/*`],
     },
   ],
 });
 
 const tracksToUpload = await readdir(__dirname + '/music');
+const backgroundAudioToUpload = await readdir(__dirname + '/backgrounds');
 
 console.log('music to add', tracksToUpload);
+console.log('background audio to add', backgroundAudioToUpload);
 
-const client = new Client({
-  endPoint: 'localhost' as string,
-  port: 9000,
-  useSSL: false,
+const localRun = (process.env.MINIO_URL as string).includes('localhost');
+
+let clientConfig: ClientOptions = {
+  endPoint: process.env.MINIO_URL as string,
+  useSSL: !localRun,
   accessKey: process.env.MINIO_ACCESS_KEY as string,
   secretKey: process.env.MINIO_SECRET_KEY as string,
-});
-
-const makeMusicBucket = async () => {
-  const bucketExists = await client.bucketExists('music');
-
-  if (bucketExists) {
-    console.log('\nmusic bucket already exists');
-  } else {
-    await client.makeBucket('music');
-    console.log('\nmusic bucket created');
-  }
-
-  await client.setBucketPolicy('music', publicPolicy);
 };
-const getMusic = async () => {
+
+if (localRun) {
+  clientConfig = { ...clientConfig, port: 9000 };
+}
+
+const client = new Client(clientConfig);
+
+const buckets = ['music', 'backgrounds'];
+const makeBuckets = async () => {
+  for (let i = 0; i < buckets.length; i++) {
+    const bucketExists = await client.bucketExists(buckets[i]);
+    if (bucketExists) {
+      console.log(`\n${buckets[i]} bucket already exists`);
+    } else {
+      await client.makeBucket(buckets[i]);
+      console.log(`\n${buckets[i]} bucket created`);
+    }
+    await client.setBucketPolicy(
+      buckets[i],
+      JSON.stringify(publicPolicyFactory(buckets[i]))
+    );
+    console.log(`\n${buckets[i]} bucket policy added`);
+  }
+};
+const getBucketContent = async (bucketName: string) => {
   return new Promise<BucketItem[]>((res, rej) => {
     const data: BucketItem[] = [];
-    const music = client.listObjects('music');
+    const music = client.listObjects(bucketName);
 
     music.on('data', (obj) => data.push(obj));
     music.on('error', (err) => {
@@ -67,22 +81,31 @@ const getMusic = async () => {
   });
 };
 
-const uploadMusic = async () => {
-  const promises = tracksToUpload.map((trackName) =>
-    client.fPutObject('music', trackName, `${__dirname}/music/${trackName}`)
-  );
+const uploadAudio = async () => {
+  for (let i = 0; i < buckets.length; i++) {
+    const tracksToUpload = await readdir(`${__dirname}/${buckets[i]}`);
 
-  try {
-    await Promise.all(promises);
-    console.log('tracks uploaded successfully');
-  } catch (e) {
-    console.error('track upload failed', e);
+    const promises = tracksToUpload.map((trackName) =>
+      client.fPutObject(
+        buckets[i],
+        trackName,
+        `${__dirname}/${buckets[i]}/${trackName}`
+      )
+    );
+
+    try {
+      await Promise.all(promises);
+      console.log(
+        `\n${promises.length} track${
+          promises.length > 1 ? 's' : ''
+        } uploaded successfully to bucket: ${buckets[i]}`
+      );
+    } catch (e) {
+      console.error('\ntrack upload failed', e);
+    }
   }
 };
 
-await makeMusicBucket();
+await makeBuckets();
 
-await uploadMusic();
-
-const music = await getMusic();
-console.log('\nbucket content', music);
+await uploadAudio();
