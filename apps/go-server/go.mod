6a1e6260cc8f64552a988fb302e8e2286e582dc5
Harri Evans <hevans9000@gmail.com>
2024-05-20 10:54:42 +0100

    feat(go-server): working resource gen

--- apps/go-server/go.mod
+++ apps/go-server/go.mod
@@ -10,7 +10,11 @@ require (
 )
 
 require (
+	github.com/fatih/color v1.17.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/lib/pq v1.10.9 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/sqlc-dev/pqtype v0.3.0 // indirect
+	golang.org/x/sys v0.18.0 // indirect
 )
--- apps/go-server/main.go
+++ apps/go-server/main.go
@@ -4,11 +4,14 @@ import (
 	"context"
 	"embed"
 	"fmt"
-	"html/template"
 	"log"
 	"net/http"
+	"time"
+
+	"html/template"
 	"os"
 
+	"github.com/fatih/color"
 	"github.com/joho/godotenv"
 
 	"idleversedata"
@@ -30,20 +33,34 @@ func main() {
 	}
 
 	db := idleversedata.Connect()
+	defer db.Close()
 
 	queries := idleversedata.New(db)
 	ctx := context.Background()
 
-	data, err := queries.GetGenerators(ctx)
+	ticker := time.NewTicker(1 * time.Second)
 
-	// this will close the connection if this functional scope panics or returns
-	defer db.Close()
+	defer ticker.Stop() // Ensure the ticker is stopped when you're done with it
 
-	if err != nil {
-		panic(err)
-	}
+	// Channel to signal when to stop the ticker
+	done := make(chan bool)
 
-	fmt.Print(data)
+	// Goroutine to handle the periodic task
+	go func() {
+		for {
+			select {
+			case t := <-ticker.C:
+				// This block runs every time the ticker ticks (every second)
+				generateResources(ctx, queries, t)
+			case <-done:
+				// Exit the goroutine when done signal is received
+				fmt.Println("Stopping the ticker")
+				return
+			}
+		}
+	}()
+
+	// Run an infinite loop that handles the ticks
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		data := map[string]string{
@@ -55,4 +72,18 @@ func main() {
 
 	log.Println("listening on", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
+
+}
+
+func generateResources(ctx context.Context, queries *idleversedata.Queries, t time.Time) {
+
+	green := color.New(color.FgGreen).SprintFunc()
+	formattedTime := t.Format("2006-01-02 15:04:05")
+	start := time.Now()
+	err := queries.RunGeneration(ctx)
+	if err != nil {
+		panic(err)
+	}
+	duration := time.Since(start)
+	fmt.Printf("%s: Resources generated in %s \n", formattedTime, green(duration))
 }
